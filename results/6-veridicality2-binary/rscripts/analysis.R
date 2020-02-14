7cdb791d82c00cde89899618e6a8b47097ea2c01
thegricean <judithdegen@gmail.com>
2020-02-14 00:04:13 -0800

    added exp 3b results and plot

--- results/6-veridicality2-binary/rscripts/analysis.R
+++ results/6-veridicality2-binary/rscripts/analysis.R
@@ -38,7 +38,7 @@ saveRDS(model.brms.proj.b, "../models/brm_model.rds")
 # create LaTeX table
 mcmcReg(model.brms.proj.b, pars = "b_", file="../models/brm_output.tex")
 
-# the way to do pairwise comparisons, if we want them:
+# the way to do pairwise comparisons, if we want them (but all probs of differing from contradictory control are 1):
 q = c(q_know_entailing = "verbknow = 0",
       q_see_entailing = "verbsee = 0",
       q_discover_entailing = "verbdiscover = 0",
@@ -47,11 +47,11 @@ q = c(q_know_entailing = "verbknow = 0",
 q_answer = hypothesis(model.brms.proj.b, q)
 q_answer
 plot(q_answer)
-prop.table(table(q_answer$samples$H1 < 0)) # p(know < entailing) = .69, ie, not very high
-prop.table(table(q_answer$samples$H2 < 0)) # p(see < entailing) = .81, ie, high but not within the 95% credible interval
-prop.table(table(q_answer$samples$H3 < 0)) # p(discover < entailing) = .81, ie, high but not within the 95% credible interval
-prop.table(table(q_answer$samples$H4 < 0)) # p(confirm < entailing) = .949, ie, very high but still not within the 95% credible interval
-prop.table(table(q_answer$samples$H5 < 0)) # p(be_annoyed < entailing) = .999, ie, very high, clear evidence that it's different
+prop.table(table(q_answer$samples$H1 < 0)) 
+prop.table(table(q_answer$samples$H2 < 0)) 
+prop.table(table(q_answer$samples$H3 < 0)) 
+prop.table(table(q_answer$samples$H4 < 0)) 
+prop.table(table(q_answer$samples$H5 < 0)) 
 
 
 # to load saved model:
