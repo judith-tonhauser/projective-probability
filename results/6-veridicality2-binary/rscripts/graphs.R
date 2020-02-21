d89b1a1c6f0084b3723ec19c6fcada959b727a75
judith-tonhauser <tonhauser.1@osu.edu>
2020-02-21 09:54:07 +0100

    worked on paper section 3 and updated one graph

--- results/6-veridicality2-binary/rscripts/graphs.R
+++ results/6-veridicality2-binary/rscripts/graphs.R
@@ -75,7 +75,7 @@ ggplot(prop, aes(x=verb, y=Mean, fill=VeridicalityGroup, shape=VeridicalityGroup
   geom_jitter(data=cd,aes(y=jittery),shape=1,color="gray40",alpha=.2,fill="black",height=.2,width=.3) +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25,color="black") +
   geom_point(stroke=.5,size=2.5,color="black") +
-  scale_shape_manual(values=c(21,25,22,24,23),labels=c("control","non-veridical\nnon-factive","veridical\nnon-factive","optionally\nfactive","factive"),name="Predicate type") +
+  scale_shape_manual(values=c(21,22,25,24,23),labels=c("control","non-veridical\nnon-factive","veridical\nnon-factive","optionally\nfactive","factive"),name="Predicate type") +
   scale_fill_manual(values=c("black","gray60","dodgerblue","tomato1","darkorchid"),labels=c("control","non-veridical\nnon-factive","veridical\nnon-factive","optionally\nfactive","factive"),name="Predicate type") +
   theme(text = element_text(size=12), axis.text.x = element_text(size = 12, angle = 45, hjust = 1, 
                                                                  color=cols$Colors)) +
@@ -169,44 +169,3 @@ ggplot(no[no$response == "No",], aes(x=verb, y=workerid, fill=VeridicalityGroup)
                                                                  color=cols$Colors))
 ggsave("../graphs/no-entailment-responses-by-pred-and-worker.pdf",height=3,width=4)
 
-# JD CODE STARTS HERE
-# TL;DR: all verbs are different from bad controls
-cd <- read.csv(file="../data/cd.csv", header=TRUE, sep=",")
-
-# Bayesian mixed effects regression to test whether ratings differ by predicate from good controls
-cd$workerid = as.factor(as.character(cd$workerid))
-
-# plotting slider ratings suggests we should use a zoib model
-ggplot(cd, aes(x=response)) +
-  geom_histogram(stat="count")
-
-# exclude bad controls from analysis -- they're not relevant, right?
-d = cd %>%
-  # filter(verb != "control_bad") %>%
-  droplevels() %>%
-  # mutate(verb = fct_relevel(verb,"contradictory C")) %>%
-  mutate(verb = fct_relevel(verb,"be_right")) %>%
-  mutate(nResponse = ifelse(response == "Yes", 1, 0))
-
-# JD removed the by-content intercepts and slopes for verb because it was taking TOO DAMN LONG and appeared to get stuck and not converge, and the reason for this is simply that the data are extreme and there is basically no item variability to speak of. barely any subject variability, too. had to 
-m <- glmer(response ~ verb + (1|workerid), data=d, family="binomial")
-# m <- glm(response ~ verb, data=d, family="binomial")
-summary(m)
-
-allm = allFit(m)
-
-is.OK <- sapply(allm,is,"merMod")  ##  failed, others succeeded
-allm.OK <- allm[is.OK]
-allm.OK
-lapply(allm.OK,function(x) x@optinfo$conv$lme4$messages)
-
-summary(allm) # bobyqa exited ok, so we can interpret model above
-summary(m)
-
-
-m <- brm(nResponse~verb + (1|workerid), data=d, family=bernoulli())
-
-# no neeed to run this multiple times
-saveRDS(m,file="../data/bernoulli-model.rds")
-
-summary(m) # see summary printed below
--- results/compare-diagnostics-and-response-tasks/rscripts/analysis.R
+++ results/compare-diagnostics-and-response-tasks/rscripts/analysis.R
@@ -1,5 +1,6 @@
-# Prior probability work
-# compare binary and gradient
+# Factives paper
+# compare findings of binary and gradient response tasks
+# compare findings of different entailment diagnostics
 
 # set working directory to directory of script
 this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
@@ -36,7 +37,7 @@ d_contr_nb = read.csv("../../2-veridicality2/data/cd.csv")
 table(d_contr_nb$verb)
 
 
-# projectivity ----
+# projection: binary vs continuous ----
 
 # for projectivity data, plot proportions against mean slider ratings
 p_prop = d_proj_b %>%
@@ -91,7 +92,7 @@ corr_projectivity = pd %>%
   summarize(Cor=cor(Prop,Mean,method="spearman"))
 corr_projectivity #.983
 
-# inference diagnostic ----
+# inference diagnostic: binary versus continuous ----
 
 # for inference entailment data, plot proportions against mean slider ratings
 einf_prop = d_inf_b %>%
@@ -152,7 +153,7 @@ corr_inference = einfd %>%
   summarize(Cor=cor(Prop,Mean,method="spearman"))
 corr_inference #.989
 
-# contradictoriness diagnostic ----
+# contradictoriness diagnostic: binary versus continuous ----
 
 # for contradictoriness entailment data, plot proportions against mean slider ratings
 econtr_prop = d_contr_b %>%
@@ -218,7 +219,7 @@ corr_contradict = econtrd %>%
   summarize(Cor=cor(Prop,Mean,method="spearman"))
 corr_contradict #.985
 
-# create joint plot ----
+# create joint binary/continuous comparison plot for appendix ----
 
 prow <- plot_grid( pp + theme(legend.position="none"),
                    pe + theme(legend.position="none"),
@@ -235,12 +236,15 @@ legend <- get_legend(pp + guides(color = guide_legend(nrow = 1)) +
 p <- plot_grid(prow, legend, ncol = 1, rel_heights = c(1, .1))
 ggsave("../graphs/joint-comparison-plot.pdf",height=4,width=9)
 
-# compare the two non-binary entailment diagnostics ----
+# compare the two gradient (non-binary = nb) entailment diagnostics ----
+table(d_inf_nb$verb)
+table(d_contr_nb$verb)
+
 einf_means = d_inf_nb %>%
   group_by(verb) %>%
   summarize(Mean = mean(response), CILow = ci.low(response), CIHigh = ci.high(response)) %>%
   mutate(YMinM = Mean - CILow, YMaxM = Mean + CIHigh) %>%
-  mutate(verb=recode(verb, control_bad = "non-ent. C", control_good = "entailing C", annoyed = "be_annoyed", be_right_that = "be_right", inform_Sam = "inform")) %>%
+  mutate(verb=recode(verb, "non-ent. C" = "non-entailing", "entailing C" = "entailing")) %>%
   select(-CILow,-CIHigh)
 einf_means 
 
@@ -248,25 +252,25 @@ einf_means2 = d_contr_nb %>%
   group_by(verb) %>%
   summarize(Mean2 = mean(response), CILow = ci.low(response), CIHigh = ci.high(response)) %>%
   mutate(YMinM2 = Mean2 - CILow, YMaxM2 = Mean2 + CIHigh) %>%
-  mutate(verb=recode(verb, control_bad = "non-ent. C", control_good = "entailing C", annoyed = "be_annoyed", be_right_that = "be_right", inform_Sam = "inform")) %>%
+  mutate(verb=recode(verb, "non-contrd. C" = "non-entailing", "contradictory C" = "entailing")) %>%
   select(-CILow,-CIHigh)
 einf_means2 
 
-e_inf_contr = einf_means %>%
+e_inf_contr_nb = einf_means %>%
   left_join(einf_means2, by = "verb") %>%
   mutate(VeridicalityGroup = factor(case_when(
     verb %in% c("know", "discover", "reveal", "see", "be_annoyed") ~ "F", 
     verb %in% c("pretend", "think", "suggest", "say") ~ "NF", 
     verb %in% c("be_right","demonstrate") ~ "VNF",
-    verb %in% c("entailing C","non-ent. C") ~ "control",
+    verb %in% c("entailing","non-entailing") ~ "control",
     TRUE ~ "V")))
-e_inf_contr
-levels(e_inf_contr$VeridicalityGroup)
+e_inf_contr_nb
+levels(e_inf_contr_nb$VeridicalityGroup)
 
-ggplot(e_inf_contr, aes(x=Mean, y=Mean2, fill=VeridicalityGroup)) +
+ggplot(e_inf_contr_nb, aes(x=Mean, y=Mean2, fill=VeridicalityGroup)) +
+  geom_point(shape=21,stroke=.5,size=2.5,color="black") +
   geom_errorbar(aes(ymin=YMinM,ymax=YMaxM),width=0) +
   geom_errorbarh(aes(xmin=YMinM2,xmax=YMaxM2),width=0) +
-  geom_point(shape=21,stroke=.5,size=2.5,color="black") +
   # scale_y_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0)) +
   # scale_alpha(range = c(.3,1)) +
   scale_fill_manual(values=c("black","darkorchid","gray60","tomato1","dodgerblue")) +
@@ -283,9 +287,66 @@ ggplot(e_inf_contr, aes(x=Mean, y=Mean2, fill=VeridicalityGroup)) +
   ylim(c(0,1))
 ggsave("../graphs/comparison-of-entailment-diagnostics.pdf",height=3,width=3)
 
-
-corr_inf_contr = e_inf_contr %>%
-  filter(VeridicalityGroup != "control") %>%
+# Spearman rank correlation on targets and controls: gradient entailment diagnostics
+corr_inf_contr_nb = e_inf_contr_nb %>%
+  #filter(VeridicalityGroup != "control") %>%
   summarize(Cor=cor(Mean,Mean2,method="spearman"))
-corr_inf_contr
+corr_inf_contr #.953
+
+# compare the two categorical (binary = b) entailment diagnostics ----
+table(d_inf_b$verb)
+table(d_contr_b$verb)
+
+einf_means = d_inf_b %>%
+  group_by(verb) %>%
+  summarize(Prop = mean(nResponse), CILow = ci.low(nResponse), CIHigh = ci.high(nResponse)) %>%
+  mutate(YMinM = Prop - CILow, YMaxM = Prop + CIHigh) %>%
+  mutate(verb=recode(verb, "non-ent. C" = "non-entailing", "entailing C" = "entailing")) %>%
+  select(-CILow,-CIHigh)
+einf_means 
+
+einf_means2 = d_contr_b %>%
+  group_by(verb) %>%
+  summarize(Prop2 = mean(nResponse), CILow = ci.low(nResponse), CIHigh = ci.high(nResponse)) %>%
+  mutate(YMinM2 = Prop2 - CILow, YMaxM2 = Prop2 + CIHigh) %>%
+  mutate(verb=recode(verb, "non-contrd. C" = "non-entailing", "contradictory C" = "entailing")) %>%
+  select(-CILow,-CIHigh)
+einf_means2 
+
+e_inf_contr_b = einf_means %>%
+  left_join(einf_means2, by = "verb") %>%
+  mutate(VeridicalityGroup = factor(case_when(
+    verb %in% c("know", "discover", "reveal", "see", "be_annoyed") ~ "F", 
+    verb %in% c("pretend", "think", "suggest", "say") ~ "NF", 
+    verb %in% c("be_right","demonstrate") ~ "VNF",
+    verb %in% c("entailing","non-entailing") ~ "control",
+    TRUE ~ "V")))
+e_inf_contr_b
+levels(e_inf_contr_b$VeridicalityGroup)
+
+ggplot(e_inf_contr_b, aes(x=Prop, y=Prop2, fill=VeridicalityGroup)) +
+  geom_point(shape=21,stroke=.5,size=2.5,color="black") +
+  geom_errorbar(aes(ymin=YMinM,ymax=YMaxM),width=0) +
+  geom_errorbarh(aes(xmin=YMinM2,xmax=YMaxM2),width=0) +
+  # scale_y_continuous(limits = c(0,1),breaks = c(0,0.2,0.4,0.6,0.8,1.0)) +
+  # scale_alpha(range = c(.3,1)) +
+  scale_fill_manual(values=c("black","darkorchid","gray60","tomato1","dodgerblue")) +
+  geom_abline(intercept=0,slope=1,color="gray70",linetype="dashed") +
+  # guides(fill=FALSE) +
+  # theme(text = element_text(size=12), axis.text.x = element_text(size = 12, angle = 45, hjust = 1, 
+  # color=cols$Colors)) +
+  # theme(legend.position="top") +
+  ylab("Proportion of 'yes (def. follows)' ratings") +
+  xlab("Mean inference rating") +
+  guides(fill=FALSE) +
+  coord_fixed(ratio = 1) +
+  xlim(c(0,1)) +
+  ylim(c(0,1))
+ggsave("../graphs/comparison-of-entailment-diagnostics.pdf",height=3,width=3)
+
+# Spearman rank correlation on targets and controls: binary entailment diagnostics
+corr_inf_contr_b = e_inf_contr_b %>%
+  #filter(VeridicalityGroup != "control") %>%
+  summarize(Cor=cor(Prop,Prop2,method="spearman"))
+corr_inf_contr_b #.934
 
