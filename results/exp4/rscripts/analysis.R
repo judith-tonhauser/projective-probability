d06589332e6ad03c9f7468e608cf8b130f2732d9
judith-tonhauser <tonhauser.1@osu.edu>
2020-08-20 18:34:58 +0200

    added SALT paper info

--- results/exp4/rscripts/analysis.R
+++ results/exp4/rscripts/analysis.R
@@ -17,6 +17,7 @@ source('../../helpers.R')
 # load data
 d = read.csv("../data/data_preprocessed.csv")
 nrow(d) #14872
+names(d)
 
 # prepare for spreading: rename the 'prior' column into 'prior_type'
 colnames(d)[colnames(d)=="prior"] = "prior_type"
@@ -26,7 +27,7 @@ colnames(d)[colnames(d)=="prior"] = "prior_type"
 d_nomc = droplevels(subset(d, short_trigger != "MC"))
 nrow(d_nomc) #11440 / 286 turkers = 40 target stimuli per Turker (20 proj, 20 prior)
 
-# spread responses over separate columns for projectivity, at-issueness and prior probability
+# spread responses over separate columns for projectivity and prior probability
 t_nomc = d_nomc %>%
   mutate(block_proj = ifelse(question_type=="projective"&block=="block1", "block1", 
                              ifelse(question_type=="projective"&block=="block2","block2",
@@ -40,9 +41,35 @@ nrow(t_nomc) #5720 = 286 turkers x 20 rows
 t_nomc = cbind(t_nomc,myCenter(t_nomc[,c("prior","projective","block_proj")]))
 summary(t_nomc)
 
+# is the CC of every predicate projective, compared to the main clause content? (SALT 2020)-----
+nrow(d) # data with main clauses
+names(d)
+table(d$content)
+
+# spread responses over separate columns for projectivity, at-issueness and prior probability
+d = d %>%
+  mutate(block_proj = ifelse(question_type=="projective"&block=="block1", "block1", 
+                             ifelse(question_type=="projective"&block=="block2","block2",
+                                    ifelse(question_type=="prior"&block=="block1","block2","block1")))) %>%
+  select(workerid,content,short_trigger,question_type,response,prior_type,block_proj) %>%     # 'event' (CC) is missing... (?)
+  spread(question_type,response) %>%
+  unite(item,short_trigger,content,remove=F)
+nrow(d) #7436 = 14872 / 2
+
+d$item = as.factor(paste(d$short_trigger,d$content))
+
+# LME model predicting rating from predicate
+table(d$short_trigger)
+table(d$prior)
+names(d)
+d$short_trigger <- relevel(d$short_trigger, ref = "MC")
+m = lmer(projective ~ short_trigger + prior + (1+short_trigger|workerid) + (1|item), data = d, REML=F)
+summary(m)
+
+
 # does prior predict projection?
 # this model accounts for the fact that we've seen by-predicate variability, both in projection and in effect of prior on projection
-model = lmer(projective ~ cprior  + (1+cprior|workerid) + (1|content) + (1+cprior|short_trigger), data = t_nomc, REML=F)
+model = lmer(projective ~ cprior  + (1+cprior|workerid) + (1|content) + (1+cprior|short_trigger), data = d, REML=F)
 summary(model)
 
 # compare to model with block interaction, to identify whether block order mattered
