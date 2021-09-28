8c35354d441799b1edc2c77dfc9c56e0b41163a2
thegricean <judithdegen@gmail.com>
2021-09-28 09:55:09 -0700

    added predicate-specific guassians

--- results/5-projectivity-no-fact/rscripts/mixture.R
+++ results/5-projectivity-no-fact/rscripts/mixture.R
@@ -28,6 +28,9 @@ cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00",
 cd = read.csv("../data/cd.csv") %>% 
   mutate(verb=as.factor(verb))
 
+cd = cd %>% 
+  mutate(verb = fct_recode(verb, "be-annoyed"="be_annoyed", "be-right"="be_right"))
+
 # first, because response assumes values of 0 and 1, which beta regression cannot handle, transform: (Smithson & Verkuilen 2006)
 # y'' = (y' ?? (n ??? 1) + 0.5)/n
 # note: first rescaling of y'=(y-a)/(b-a) not necessary because highest and lowest value are 0 and 1 already
@@ -311,140 +314,140 @@ ggsave(filename,width=4,height=2.5)
 
 
 
-# plot best mixture for "reveal"
-dsub = cd %>% 
-  filter(verb == "reveal")
-
-# 1.  test how many Gaussian components are justified
-mclust = Mclust(dsub$betaresponse)
-
-# print results to console
-print(summary(mclust))
-
-colors = cbPalette
-
-# plot optimal number of clusters
-mixmdl <- normalmixEM(dsub$betaresponse, k = mclust$G)
-responses <- data.frame(x=mixmdl$x)
-
-baseplot = ggplot(responses) +
-  geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white")
-
-print("mean")
-print(mean(dsub$betaresponse))
-
-for (i in 1:mclust$G)
-{
-  print(paste("component ",i))
-  print("mu")
-  print(mixmdl$mu[i])
-  print("sigma")
-  print(mixmdl$sigma[i])
-  print("lambda")
-  print(mixmdl$lambda[i])
-  baseplot = baseplot +
-    stat_function(geom = "line", fun = plot_mix_comps_normal,
-                  args = list(mu = mixmdl$mu[i], sigma = mixmdl$sigma[i], lam = mixmdl$lambda[i]),
-                  colour = cbPalette[i], size = 1)
-  
-}
-baseplot = baseplot +
-  ylab("Density") + 
-  xlab("Slider value") +
-  scale_x_continuous(breaks=seq(0,1,by=.1))
-filename = paste("../graphs/mixtures/example-4components-reveal.pdf",sep="")
-ggsave(filename,width=4,height=2.5)
-
-# plot best mixture for "discover"
-dsub = cd %>% 
-  filter(verb == "discover")
-
-# 1.  test how many Gaussian components are justified
-mclust = Mclust(dsub$betaresponse)
-
-# print results to console
-print(summary(mclust))
-
-colors = cbPalette
-
-# plot optimal number of clusters
-mixmdl <- normalmixEM(dsub$betaresponse, k = mclust$G)
-responses <- data.frame(x=mixmdl$x)
-
-baseplot = ggplot(responses) +
-  geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white")
-
-print("mean")
-print(mean(dsub$betaresponse))
-
-for (i in 1:mclust$G)
-{
-  print(paste("component ",i))
-  print("mu")
-  print(mixmdl$mu[i])
-  print("sigma")
-  print(mixmdl$sigma[i])
-  print("lambda")
-  print(mixmdl$lambda[i])
-  baseplot = baseplot +
-    stat_function(geom = "line", fun = plot_mix_comps_normal,
-                  args = list(mu = mixmdl$mu[i], sigma = mixmdl$sigma[i], lam = mixmdl$lambda[i]),
-                  colour = cbPalette[i], size = 1)
-  
-}
-baseplot = baseplot +
-  ylab("Density") + 
-  xlab("Slider value") +
-  scale_x_continuous(breaks=seq(0,1,by=.1))
-filename = paste("../graphs/mixtures/example-3components-discover.pdf",sep="")
-ggsave(filename,width=4,height=2.5)
-
-# simulate idealized 2-component data with different mixtures
-simdata = data.frame(simvals = c(rnorm(90,mean=0.1,sd=0.03),rnorm(176,mean=0.9,sd=0.03)))
-simdata = simdata %>% 
-  mutate(simvals_norm = case_when(simvals < 0 ~ 0, # force vals <0 to 0
-                                  simvals > 1 ~ 1, # force vals >1 to 1
-                                  TRUE ~ simvals))
-
-mclust = Mclust(simdata$simvals_norm)
-
-# print results to console
-print(summary(mclust))
-
-# update data frame to indicate optimal number of gaussian clusters
-colors = cbPalette
-
-# plot optimal number of clusters
-mixmdl <- normalmixEM(simdata$simvals_norm, k = mclust$G)
-responses <- data.frame(x=mixmdl$x)
-
-# component 1
-mixmdl$mu[1]
-mixmdl$sigma[1]
-mixmdl$lambda[1]
-
-# component 2
-mixmdl$mu[2]
-mixmdl$sigma[2]
-mixmdl$lambda[2]
-
-# mean
-mean(simdata$simvals_norm)
-
-ggplot(responses) +
-  geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white") +
-  stat_function(geom = "line", fun = plot_mix_comps_normal,
-                args = list(mu = mixmdl$mu[1], sigma = mixmdl$sigma[1], lam = mixmdl$lambda[1]),
-                colour = cbPalette[1], size = 1) +
-  stat_function(geom = "line", fun = plot_mix_comps_normal,
-                args = list(mu = mixmdl$mu[2], sigma = mixmdl$sigma[2], lam = mixmdl$lambda[2]),
-                colour = cbPalette[2], size = 1) +
-  ylab("Density") + 
-  xlab("Slider value") +
-  scale_x_continuous(breaks=seq(0,1,by=.1))
-
-filename = paste("../graphs/mixtures/example_2components.pdf",sep="")
-ggsave(filename,width=5,height=3)
+# # plot best mixture for "reveal"
+# dsub = cd %>% 
+#   filter(verb == "reveal")
+# 
+# # 1.  test how many Gaussian components are justified
+# mclust = Mclust(dsub$betaresponse)
+# 
+# # print results to console
+# print(summary(mclust))
+# 
+# colors = cbPalette
+# 
+# # plot optimal number of clusters
+# mixmdl <- normalmixEM(dsub$betaresponse, k = mclust$G)
+# responses <- data.frame(x=mixmdl$x)
+# 
+# baseplot = ggplot(responses) +
+#   geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white")
+# 
+# print("mean")
+# print(mean(dsub$betaresponse))
+# 
+# for (i in 1:mclust$G)
+# {
+#   print(paste("component ",i))
+#   print("mu")
+#   print(mixmdl$mu[i])
+#   print("sigma")
+#   print(mixmdl$sigma[i])
+#   print("lambda")
+#   print(mixmdl$lambda[i])
+#   baseplot = baseplot +
+#     stat_function(geom = "line", fun = plot_mix_comps_normal,
+#                   args = list(mu = mixmdl$mu[i], sigma = mixmdl$sigma[i], lam = mixmdl$lambda[i]),
+#                   colour = cbPalette[i], size = 1)
+#   
+# }
+# baseplot = baseplot +
+#   ylab("Density") + 
+#   xlab("Slider value") +
+#   scale_x_continuous(breaks=seq(0,1,by=.1))
+# filename = paste("../graphs/mixtures/example-4components-reveal.pdf",sep="")
+# ggsave(filename,width=4,height=2.5)
+# 
+# # plot best mixture for "discover"
+# dsub = cd %>% 
+#   filter(verb == "discover")
+# 
+# # 1.  test how many Gaussian components are justified
+# mclust = Mclust(dsub$betaresponse)
+# 
+# # print results to console
+# print(summary(mclust))
+# 
+# colors = cbPalette
+# 
+# # plot optimal number of clusters
+# mixmdl <- normalmixEM(dsub$betaresponse, k = mclust$G)
+# responses <- data.frame(x=mixmdl$x)
+# 
+# baseplot = ggplot(responses) +
+#   geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white")
+# 
+# print("mean")
+# print(mean(dsub$betaresponse))
+# 
+# for (i in 1:mclust$G)
+# {
+#   print(paste("component ",i))
+#   print("mu")
+#   print(mixmdl$mu[i])
+#   print("sigma")
+#   print(mixmdl$sigma[i])
+#   print("lambda")
+#   print(mixmdl$lambda[i])
+#   baseplot = baseplot +
+#     stat_function(geom = "line", fun = plot_mix_comps_normal,
+#                   args = list(mu = mixmdl$mu[i], sigma = mixmdl$sigma[i], lam = mixmdl$lambda[i]),
+#                   colour = cbPalette[i], size = 1)
+#   
+# }
+# baseplot = baseplot +
+#   ylab("Density") + 
+#   xlab("Slider value") +
+#   scale_x_continuous(breaks=seq(0,1,by=.1))
+# filename = paste("../graphs/mixtures/example-3components-discover.pdf",sep="")
+# ggsave(filename,width=4,height=2.5)
+# 
+# # simulate idealized 2-component data with different mixtures
+# simdata = data.frame(simvals = c(rnorm(90,mean=0.1,sd=0.03),rnorm(176,mean=0.9,sd=0.03)))
+# simdata = simdata %>% 
+#   mutate(simvals_norm = case_when(simvals < 0 ~ 0, # force vals <0 to 0
+#                                   simvals > 1 ~ 1, # force vals >1 to 1
+#                                   TRUE ~ simvals))
+# 
+# mclust = Mclust(simdata$simvals_norm)
+# 
+# # print results to console
+# print(summary(mclust))
+# 
+# # update data frame to indicate optimal number of gaussian clusters
+# colors = cbPalette
+# 
+# # plot optimal number of clusters
+# mixmdl <- normalmixEM(simdata$simvals_norm, k = mclust$G)
+# responses <- data.frame(x=mixmdl$x)
+# 
+# # component 1
+# mixmdl$mu[1]
+# mixmdl$sigma[1]
+# mixmdl$lambda[1]
+# 
+# # component 2
+# mixmdl$mu[2]
+# mixmdl$sigma[2]
+# mixmdl$lambda[2]
+# 
+# # mean
+# mean(simdata$simvals_norm)
+# 
+# ggplot(responses) +
+#   geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white") +
+#   stat_function(geom = "line", fun = plot_mix_comps_normal,
+#                 args = list(mu = mixmdl$mu[1], sigma = mixmdl$sigma[1], lam = mixmdl$lambda[1]),
+#                 colour = cbPalette[1], size = 1) +
+#   stat_function(geom = "line", fun = plot_mix_comps_normal,
+#                 args = list(mu = mixmdl$mu[2], sigma = mixmdl$sigma[2], lam = mixmdl$lambda[2]),
+#                 colour = cbPalette[2], size = 1) +
+#   ylab("Density") + 
+#   xlab("Slider value") +
+#   scale_x_continuous(breaks=seq(0,1,by=.1))
+# 
+# filename = paste("../graphs/mixtures/example_2components.pdf",sep="")
+# ggsave(filename,width=5,height=3)
 
 
 
@@ -477,7 +480,7 @@ for (p in levels(cd$verb)) {
   responses <- data.frame(x=mixmdl$x)
   
   baseplot = ggplot(responses) +
-    geom_histogram(aes(x, ..density..), binwidth = .01, colour = "black", fill = "white")
+    geom_histogram(aes(x, ..density..), binwidth = .02, colour = "black", fill = "white")
   
   for (i in 1:mclust$G)
   {
@@ -487,50 +490,52 @@ for (p in levels(cd$verb)) {
                   colour = cbPalette[i], size = 1)
     
   }
-  title = paste("\'",p,"\': mixed Gaussian model with ",mclust$G, " components",sep="")
+  # title = paste("\'",p,"\': mixed Gaussian model with ",mclust$G, " components",sep="")
+  title = p
   baseplot = baseplot +
+    # geom_label(aes(x=0.5,y=10,label=p)) +
     ylab("Density") + 
     xlab("Slider value") +
     scale_x_continuous(breaks=seq(0,1,by=.1)) + 
     ggtitle(title)
-  filename = paste("../graphs/mixtures/",p,"_gaussian.pdf",sep="")
-  ggsave(filename)
+  filename = paste("../graphs/mixtures/",p,"-gaussian.pdf",sep="")
+  ggsave(filename,width=4,height=3)
   
   ######################################################
   # 2.  plot mixtures of varying number of beta components
-  for (n in 1:3) {
-    # fit the model with components == n
-    betamdl = BM_Fit(dsub$betaresponse, n, .01)
-    print(p)
-    print(betamdl)
-
-    # extract the model parameters
-    params = data.frame()
-    for (j in 1:n) {
-      params[1,paste("alpha_",j,sep="")] = betamdl$Iterations[nrow(betamdl$Iterations),paste("Alpha_",j,sep="")]
-      params[1,paste("beta_",j,sep="")] = betamdl$Iterations[nrow(betamdl$Iterations),paste("Beta_",j,sep="")]
-      params[1,paste("mix_",j,sep="")] = betamdl$Iterations[nrow(betamdl$Iterations),paste("Mixture_Param_",j,sep="")]
-    }
-    
-    # plot the inferred beta distributions
-    baseplot = ggplot(dsub) +
-      geom_histogram(aes(x=betaresponse, y=..density..), binwidth = .005, colour = "black", fill = "white")
-
-    baseplot = baseplot +
-      stat_function(fun = function(x) params$mix_1*0.3*dbeta(x, params$alpha_1, params$beta_1), color = cbPalette[1], size = 1) +
-      stat_function(fun = function(x) params$mix_2*0.3*dbeta(x, params$alpha_2, params$beta_2), color = cbPalette[2], size = 1) +
-      stat_function(fun = function(x) params$mix_3*0.3*dbeta(x, params$alpha_3, params$beta_3), color = cbPalette[3], size = 1)
-    
-    title = paste("\'",p,"\': mixed Beta model with ",n, " components",sep="")
-    baseplot = baseplot +
-      ylab("Density") + 
-      xlab("Slider value") +
-      scale_x_continuous(breaks=seq(0,1,by=.1)) +
-      ggtitle(title)
-    filename = paste("../graphs/mixtures/",p,"_beta_",n,".pdf",sep="")
-    ggsave(filename)
-
-    }
+  # for (n in 1:3) {
+  #   # fit the model with components == n
+  #   betamdl = BM_Fit(dsub$betaresponse, n, .01)
+  #   print(p)
+  #   print(betamdl)
+  # 
+  #   # extract the model parameters
+  #   params = data.frame()
+  #   for (j in 1:n) {
+  #     params[1,paste("alpha_",j,sep="")] = betamdl$Iterations[nrow(betamdl$Iterations),paste("Alpha_",j,sep="")]
+  #     params[1,paste("beta_",j,sep="")] = betamdl$Iterations[nrow(betamdl$Iterations),paste("Beta_",j,sep="")]
+  #     params[1,paste("mix_",j,sep="")] = betamdl$Iterations[nrow(betamdl$Iterations),paste("Mixture_Param_",j,sep="")]
+  #   }
+  #   
+  #   # plot the inferred beta distributions
+  #   baseplot = ggplot(dsub) +
+  #     geom_histogram(aes(x=betaresponse, y=..density..), binwidth = .005, colour = "black", fill = "white")
+  # 
+  #   baseplot = baseplot +
+  #     stat_function(fun = function(x) params$mix_1*0.3*dbeta(x, params$alpha_1, params$beta_1), color = cbPalette[1], size = 1) +
+  #     stat_function(fun = function(x) params$mix_2*0.3*dbeta(x, params$alpha_2, params$beta_2), color = cbPalette[2], size = 1) +
+  #     stat_function(fun = function(x) params$mix_3*0.3*dbeta(x, params$alpha_3, params$beta_3), color = cbPalette[3], size = 1)
+  #   
+  #   title = paste("\'",p,"\': mixed Beta model with ",n, " components",sep="")
+  #   baseplot = baseplot +
+  #     ylab("Density") + 
+  #     xlab("Slider value") +
+  #     scale_x_continuous(breaks=seq(0,1,by=.1)) +
+  #     ggtitle(title)
+  #   filename = paste("../graphs/mixtures/",p,"_beta_",n,".pdf",sep="")
+  #   ggsave(filename)
+  # 
+  #   }
 }
 
 optimalgaussians
